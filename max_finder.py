443e2eb58a8173c60a73eab1217adcf9d7359a99
Daniil <7102771@gmail.com>
2024-12-04 00:25:10 +0300

    max_finder.py::modified - array

--- max_finder.py
+++ max_finder.py
@@ -1,4 +1,12 @@
 def find_max(numbers):
-    if not numbers:
-        return None
+  if not numbers:
+    return None
+  else:
     return max(numbers)
+  numbers = [float(x) for x in input_str.split()]
+  max_value = find_max(numbers)
+  if max_value is None:
+    print("The list is empty.")
+  else:
+    print("The maximum value is:", max_value)
+
